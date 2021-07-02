0cd09461f11d69efa092b5fc9e3afe716e004f7e
James <thisisjameslyon@gmail.com>
2021-07-02 15:29:06 +1000

    added hover event listener

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 `use strict`;
 
 const grid = document.getElementById(`grid`)
+const gridReset = document.getElementById(`gridReset`)
 
 for(let i = 1; i<=256; i++){
 
@@ -8,3 +9,17 @@ for(let i = 1; i<=256; i++){
  gridItem.classList.add(`gridItem`)
  grid.appendChild(gridItem)
 }
+
+let gridItems = document.querySelectorAll(`.gridItem`)
+
+gridItems.forEach((gridItem)=>{
+    gridItem.addEventListener(`mouseover`,()=>{
+        gridItem.classList.add(`hovered`)
+    })
+})
+
+gridReset.addEventListener(`click`,()=>{
+    gridItems.forEach((gridItem) => {
+        gridItem.classList.remove(`hovered`)
+    })
+})
